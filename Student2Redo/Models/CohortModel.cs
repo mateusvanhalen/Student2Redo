8a2449c6c0763ddd58f628df0884411094697c6a
Matthew Hall <mateusvanhalen@gmail.com>
2018-12-03 14:09:25 -0600

    first commit

---
+++ Student2Redo/Models/CohortModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student2Redo.Models
+{
+    public class CohortModel
+    {
+    }
+}
